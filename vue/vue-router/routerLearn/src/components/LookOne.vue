891207e847c52c722ce17ba1f8490a2ac851fb83
ShiningPeng <1494228462@qq.com>
2019-05-31 17:08:44 +0800

    vue-router

---
+++ vue/vue-router/routerLearn/src/components/LookOne.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="look-one">{{msg}}
+    <div class="go-two" @click='goTwo'>去look2</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'LookOne',
+  data () {
+    return {
+      msg:'怎么怎么了，'
+    }
+  },
+  methods: {
+    goTwo() {
+      // this.$router.push('/look/look-two')
+      // this.$router.push({ path: '/look/look-two'})
+      this.$router.push({name:'LookTwo',param:{ id: 12345}})
+    }
+  },
+  beforeRouteEnter (to, from, next) {
+    // 类似于生命周期函数，在路由跳转的时候产生
+    console.log(from, to);
+    next();
+
+  },
+  beforeRouteLeave (to, from, next) {
+    // ...
+    console.log(from, to);
+  }
+}
+</script>
