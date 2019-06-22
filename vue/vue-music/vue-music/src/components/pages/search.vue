81505d4adeb0bc84c6a7df57c15401a481b1872b
ShiningPeng <1494228462@qq.com>
2019-06-22 21:17:55 +0800

    vue-music

---
+++ vue/vue-music/vue-music/src/components/pages/search.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="search">
+    <div class="search-box-wrapper">
+<!-- 搜索框 -->
+
+    </div>
+  </div>
+</template>
+
+<script>
+import searchBox from '@/components/searchBox'
+export default {
+  components:{
+    'v-search-box':searchBox
+  }
+}
+</script>
---
+++ vue/vue-music/vue-music/src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import searchBox from '@/components/searchBox'
+
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/search',
+      name: 'searchBox',
+      component: searchBox
+    }
+  ]
+})
