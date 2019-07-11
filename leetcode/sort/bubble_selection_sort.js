7d5ef4c75be09708bf0f4196abfb7f113e2340ca
ShiningPeng <1494228462@qq.com>
2019-07-11 16:31:43 +0800

    sort algorighm

---
+++ leetcode/sort/bubble_selection_sort.js
@@ -0,0 +1,32 @@
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+function selectionSort(arr) {
+  var len = arr.length;
+  var minIndex, temp;
+  for (var i = 0; i < len; i++) {
+    minIndex = i;
+    for (var j = i + 1; j < len; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    temp = arr[i];
+    arr[i] = arr[minIndex];
+    arr[minIndex] = temp;
+  }
+  return arr;
+}
+const arr = [1, 6, 4, 8, 2, 5, 9, 7];
+console.log(bubbleSort(arr));
+console.log(selectionSort(arr));
---
+++ leetcode/sort/quick_sort.js
@@ -0,0 +1,26 @@
+// 快排
+// a(一个整体) b(中间值) c(一个整体) 看做整体，就三个数排序，两边的数与中间的数进行比较
+// 递归
+
+function quickSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  // Math.round()向下取整
+  var left = [], right = [], baseDot = Math.round(arr.length / 2),
+    base = arr.splice(baseDot, 1)[0];
+  //找到中间值
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] < base) {
+      left.push(arr[i])
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  // left a
+  // base 中间值
+  // right b
+  return quickSort(left).concat([base], quickSort(right));
+}
+var arr =[1,2,65,48,31,9,-4];
+console.log(quickSort(arr));
