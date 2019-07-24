7f836513f8050a911848dc1e0a389e07d9feaa61
ShiningPeng <1494228462@qq.com>
2019-07-24 17:03:33 +0800

    update

--- interview-questions/index.js
+++ interview-questions/index.js
@@ -44,21 +44,66 @@
 
 
 // 四、
-for (var i = 0; i < 10; i++) {
-  setTimeout(() => {
-    console.log(i);
-  }, 0);
-}
+// for (var i = 0; i < 10; i++) {
+//   setTimeout(() => {
+//     console.log(i);
+//   }, 0);
+// }
 //打印10个10，
 // 要打印出0-9
 // 1.把var改为let
 // 2. 闭包,
-for (var i = 0;i<10;i++){
-  (function(i){
-    setTimeout(() => {
-      console.log(i);
-    }, 0);
-  })(i);
-}
+// for (var i = 0;i<10;i++){
+//   (function(i){
+//     setTimeout(() => {
+//       console.log(i);
+//     }, 0);
+//   })(i);
+// }
 
 // let每次循环会生成一个封闭的作用域和setTimeout绑定，而var每次循环会覆盖掉上一次的作用域
+
+// 五、for-in for-of
+// Array.prototype.methods = function(){
+//   console.log('wn');
+// }
+// var myArray = [1, 2, 3, 4, 5, 6, 7];
+// myArray.name = 'wn';
+// for(let index in myArray){
+//   console.log(myArray[index]);
+// }
+// for (let index of myArray) {
+//   console.log(index);
+// }
+
+// for-in
+// 1. index索引为字符串类型的数字，不能直接进行几何运算，
+// 2.遍历数组遍历顺序有可能不是按照实际数组的内部数据进行的
+// 3.使用for-in会遍历数组所有可枚举属性，包括原型链，所以for-in很适合遍历对象
+
+// for-of
+//for-in 遍历的是数组的索引，for-of遍历的是数组的元素
+// for-of遍历的只是数组内的元素，而不包括数组原型属性和索引
+
+// 六、遍历arr1变成arr2
+let arr1 = [1, 2, [3, 4], 5, [2, 7, [3, 9]]];
+let arr2 = [1, 2, 3, 4, 5, 2, 7, 3, 9];
+// function outputArr(arr) {
+//   let res = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if(Array.isArray(arr[i])){
+//       res = res.concat(outputArr(arr[i]))
+//     }else{
+//       res.push(arr[i]);
+//     }
+//   }
+//   return res;
+// }
+function outputArr(arr){
+  // pre是初始值，相当于let res = []，item是数组里的每一项
+  return arr.reduce(function(pre, item){
+    return pre.concat(Array.isArray(item) ? outputArr(item) : item)
+  },[])
+}
+
+console.log(outputArr(arr1));//[ 1, 2, 3, 4, 5, 2, 7, 3, 9 ]
