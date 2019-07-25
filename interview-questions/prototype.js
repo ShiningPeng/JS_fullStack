d6dad0c7acd88ae2400b73909db0c9c6b9be8b73
ShiningPeng <1494228462@qq.com>
2019-07-25 11:38:40 +0800

    __proto__ and prototype

---
+++ interview-questions/prototype.js
@@ -0,0 +1,6 @@
+var a= {};
+var b = Object.prototype;
+console.log(a.prototype === b) //false
+console.log(a.__proto__ === b)//true
+console.log(Object.getPrototypeOf(a) === b)//true
+// 浏览器的__proto属性就相当于getPrototypeOf这个正式的api，后面两个console是一样的效果
