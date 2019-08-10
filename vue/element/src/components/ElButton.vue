46a66d534fde887fc0bbb3aa721c0b1b0876bf6b
ShiningPeng <1494228462@qq.com>
2019-08-10 16:58:43 +0800

    update

---
+++ vue/element/src/components/ElButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <button class="el-button" :disabled="buttonDisabled" 
+  :class="[
+    type?'el-button--' + type:'',
+    size?'el-button--' + size:'',
+    {
+      'is-disabled': buttonDisabled
+    }
+  ]"  @click="handleClick" 
+  >
+    <slot></slot>
+  </button>
+</template>
+<script>
+export default {
+  props:{
+    disabled: Boolean,
+    type:{
+      string:String,
+      default:'default'
+    },
+    size:String
+  },
+  methods: {
+    handleClick(evt){
+      this.$emit('click', evt);
+    }
+  },
+  computed: {
+    buttonDisabled(){
+    return  this.disabled
+    }
+  },
+}
+</script>
+<style lang="">
+  
+</style>
