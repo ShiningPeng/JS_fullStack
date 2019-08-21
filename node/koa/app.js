bc6207637684df4b4de4a0d07913990e9d697bba
ShiningPeng <1494228462@qq.com>
2019-08-21 12:01:24 +0800

    koa

---
+++ node/koa/app.js
@@ -0,0 +1,40 @@
+const Koa = require('koa');
+const fs = require('fs');
+const route = require('koa-route'); 
+const serve = require('koa-static');
+const path = require('path');
+const app = new Koa();
+// 中间件 即是函数
+// const main = ctx => {
+//   // ctx.response.body = 'hello world'
+//   // 要传文件，创建一个文件流-- 可读流、写流
+//   // ctx.response.type = 'html';
+//   // ctx.response.body = fs.createReadStream('./template.html');
+//   // if(ctx.request.path != '/') {
+//   //   ctx.response.type = 'html';
+//   //   ctx.response.body = '<a href="/">index page</a>'
+//   // }else{
+//   //   ctx.response.body = 'Hello World';
+//   // }
+// }
+const about = ctx => {
+  ctx.response.type = 'html';
+  ctx.response.body = '<a href="/">Index page</a>'
+}
+const main = ctx => {
+  ctx.response.type = 'html';
+  ctx.response.body = 'Hello Main <a href="/about">About page</a>'
+}
+const redirect = ctx => {
+  ctx.response.redirect('/');
+  ctx.response.body = '<a href="/">Index page</a>'; //这里不会执行，因为直接跳转到'/'了
+}
+// 中间件是串联的，一直循环执行
+
+//先是通过 加载serve的静态资源，没有匹配到之后就到 route 
+app.use(serve(path.join(__dirname, 'a')));
+app.use(route.get('/', main));  //返回一个函数
+app.use(route.get('/about', about));
+app.use(route.get('/redirect', redirect));
+app.use(main)
+app.listen(3000)
---
+++ node/koa/readme.md
@@ -0,0 +1,21 @@
+- SQL
+  CREATE INSERT UPDATE DELETE
+  SELECT * FROM t-user where is_delete=1;  
+  select * from t-user where id = 123;
+  索引
+  primary key autoincrement
+  unique key 
+  数据类型
+  int(11) varchar(30)
+  tinyint(1) datetime  时间类型
+  存储类型 InnoDB Memory 放在内存之中，用于快速查询
+
+
+node项目
+最开始需要 npm init -y
+
+- http的理解
+中间件
+分层的概念
+View
+Controllor
